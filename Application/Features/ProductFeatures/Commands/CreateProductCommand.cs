75b01503336e39123580693febbc915a5127fb5b
tomaszdudkowski <tomaszszd@gmail.com>
2021-05-29 10:27:45 +0200

    Add Command CRUD operations with MediatR

---
+++ Application/Features/ProductFeatures/Commands/CreateProductCommand.cs
@@ -0,0 +1,42 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Features.ProductFeatures.Commands
+{
+    public class CreateProductCommand : IRequest<int>
+    {
+        public string Name { get; set; }
+        public string Barcode { get; set; }
+        public string Description { get; set; }
+        public decimal Rate { get; set; }
+
+        public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, int>
+        {
+
+            private readonly IApplicationDbContext _context;
+
+            public CreateProductCommandHandler(IApplicationDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<int> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+            {
+                var product = new Product();
+                product.Barcode = request.Barcode;
+                product.Name = request.Name;
+                product.Rate = request.Rate;
+                product.Description = request.Description;
+                _context.Products.Add(product);
+                await _context.SaveChanges();
+                return product.ID;
+            }
+        }
+    }
+}
---
+++ Application/Features/ProductFeatures/Commands/DeleteProductByIdCommand.cs
@@ -0,0 +1,38 @@
+﻿using Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Features.ProductFeatures.Commands
+{
+    public class DeleteProductByIdCommand : IRequest<int>
+    {
+        public int ID { get; set; }
+        public class DeleteProductByIdCommandHandler : IRequestHandler<DeleteProductByIdCommand, int>
+        {
+            private readonly IApplicationDbContext _context;
+            public DeleteProductByIdCommandHandler(IApplicationDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<int> Handle(DeleteProductByIdCommand request, CancellationToken cancellationToken)
+            {
+                var product = await _context.Products.Where(p => p.ID == request.ID).FirstOrDefaultAsync();
+                if(product == null)
+                {
+                    return default; // ?? null => default
+                }
+                _context.Products.Remove(product);
+                await _context.SaveChanges();
+                return product.ID;
+            }
+        }
+
+    }
+}
