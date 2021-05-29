9754ec7cbfaa085f3ffae8994a3a522182198677
tomaszdudkowski <tomaszszd@gmail.com>
2021-05-29 09:38:28 +0200

    Add Query CRUD operations with MediatR

---
+++ Application/Features/ProductFeatures/Queries/GetProductByIdQuery.cs
@@ -0,0 +1,35 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Features.ProductFeatures.Queries
+{
+    public class GetProductByIdQuery : IRequest<Product>
+    {
+        public int ID { get; set; }
+        public class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQuery, Product>
+        {
+            private readonly IApplicationDbContext _context;
+            public GetProductByIdQueryHandler(IApplicationDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Product> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+            {
+                var product = _context.Products.Where(p => p.ID == request.ID).FirstOrDefault();
+                if(product == null)
+                {
+                    return null;
+                }
+                return product;
+            }
+        }
+    }
+}
