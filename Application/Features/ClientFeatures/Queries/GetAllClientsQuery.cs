979d070727c0ba569c802f8007083d98c624a26b
tomaszdudkowski <tomaszszd@gmail.com>
2021-06-06 12:25:28 +0200

    Add Queries for Client. Get all clients and get client.

---
+++ Application/Features/ClientFeatures/Queries/GetAllClientsQuery.cs
@@ -0,0 +1,38 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Features.ClientFeatures.Queries
+{
+    public class GetAllClientsQuery : IRequest<IEnumerable<Client>>
+    {
+        public class GetAllClientsQueryHandler : IRequestHandler<GetAllClientsQuery, IEnumerable<Client>>
+        {
+            private readonly IApplicationDbContext _context;
+
+            public GetAllClientsQueryHandler(IApplicationDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<IEnumerable<Client>> Handle(GetAllClientsQuery request, CancellationToken cancellationToken)
+            {
+                var clientList = await _context.Clients.ToListAsync();
+                
+                if(clientList == null)
+                {
+                    return null;
+                }
+
+                return clientList.AsReadOnly();
+            }
+        }
+    }
+}
---
+++ Application/Features/ClientFeatures/Queries/GetClientByIdQuery.cs
@@ -0,0 +1,37 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Features.ClientFeatures.Queries
+{
+    public class GetClientByIdQuery : IRequest<Client>
+    {
+        public int ID { get; set; }
+        public class GetClientByIdQueryHandler : IRequestHandler<GetClientByIdQuery, Client>
+        {
+            private readonly IApplicationDbContext _context;
+
+            public GetClientByIdQueryHandler(IApplicationDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Client> Handle(GetClientByIdQuery request, CancellationToken cancellationToken)
+            {
+                var client = _context.Clients.Where(c => c.ID == request.ID).FirstOrDefault();
+                if(client == null)
+                {
+                    return null;
+                }
+
+                return client;
+            }
+        }
+    }
+}
